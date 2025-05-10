333a412f6de4119b0402adf682728f2dc907d491
Andreas Kindlbacher <a.kindlbacher@gmail.com>
2025-05-10 22:59:34 +0200

    fix

---
+++ resources/views/livewire/regapp.blade.php
@@ -0,0 +1,22 @@
+<div class="container mx-auto px-4 py-8">
+    <h1 class="text-2xl font-bold mb-6 text-center text-gray-800">
+        {{ __('Fleet API Registration') }}
+    </h1>
+
+    <div class="flex flex-col gap-6">
+
+        <flux:input wire:model="appkey" :label="__('AppKey')" type="text" readonly />
+
+        <button wire:click="registerWithFleetAPI">click</button>
+            {{ __('Register with Fleet API') }}
+        </button>
+
+        @if (session('success'))
+            <div class="text-green-500">{{ session('success') }}</div>
+        @endif
+
+        @if (session('error'))
+            <div class="text-red-500">{{ session('error') }}</div>
+        @endif
+    </div>
+</div>
