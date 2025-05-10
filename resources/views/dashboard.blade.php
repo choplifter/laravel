e69ca94dbf95bc861f348e8f8472bf71899a06dd
Andreas Kindlbacher <a.kindlbacher@gmail.com>
2025-05-10 20:18:10 +0200

    counter removed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 
-                @livewire('counter')
+               <!-- @livewire('counter') -->
             </div>
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 
@@ -11,7 +11,7 @@
             </div>
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-                
+
                 @livewire('tesla.regendpoint')
             </div>
         </div>
